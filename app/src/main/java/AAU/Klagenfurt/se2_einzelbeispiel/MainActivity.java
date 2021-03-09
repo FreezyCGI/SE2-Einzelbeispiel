c3e0b25053f08fb85270d4f34cdf92d3de0e944e
Fritzl <patrick.fritzl@gmx.at>
2021-03-09 18:24:37 +0100

    Übung 2.1 fertig

--- app/src/main/java/AAU/Klagenfurt/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/AAU/Klagenfurt/se2_einzelbeispiel/MainActivity.java
@@ -4,23 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.PopupWindow;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.*;
+import java.net.*;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //final Button btnAbschicken = findViewById("btnAbschicken");
         //btnAbschicken.setOnClickListener(onBtnAbschickenClicked);
+
     }
 
-    public void onBtnAbschickenClicked(View view)
+    public void onBtnAbschickenClicked(View view) throws InterruptedException
     {
-        Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_SHORT).show();
+        EditText txtInput = findViewById(R.id.txtInput);
+        String input = txtInput.getText().toString();
+
+        TestThread t = new TestThread(input);
+        t.start();
+        t.join();
+        Toast.makeText(this, t.GetResult(), Toast.LENGTH_SHORT).show();
+    }
+}
+
+class TestThread extends Thread
+{
+    String output;
+    String fromServer;
+
+    public String GetResult()
+    {
+        return fromServer;
+    }
+
+    public TestThread(String output)
+    {
+        this.output = output;
+    }
+
+    public void run()
+    {
+        try
+        {
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+            DataOutputStream outToServer = new DataOutputStream((clientSocket.getOutputStream()));
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            outToServer.writeBytes(output + '\n');
+
+            fromServer = inFromServer.readLine();
+
+            clientSocket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
