b08c0499b72df39de50a6714c4d549f388fb6148
Fritzl <patrick.fritzl@gmx.at>
2021-03-09 17:10:00 +0100

    Button onclick eingerichtet

--- app/src/main/java/AAU/Klagenfurt/se2_einzelbeispiel/MainActivity.java
+++ app/src/main/java/AAU/Klagenfurt/se2_einzelbeispiel/MainActivity.java
@@ -3,6 +3,10 @@ package AAU.Klagenfurt.se2_einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //final Button btnAbschicken = findViewById("btnAbschicken");
+        //btnAbschicken.setOnClickListener(onBtnAbschickenClicked);
+    }
+
+    public void onBtnAbschickenClicked(View view)
+    {
+        Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_SHORT).show();
     }
 }
